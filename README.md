a33b8c0426919d6e00119ee97fda3bddabe25337
mattialosz <mattia.loszach@gmail.com>
2026-01-22 13:53:22 +0100

    Base Structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# fast-inverse-square-root
+# Fast Inverse Square Root Algorithms
 Benchmarking Fast Inverse Square Root Algorithms for ARM64 Architecture
+
+## Build
+clang++ -O3 -mcpu=native main.cpp -o bench
+./bench
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <arm_neon.h>
+#include <iostream>
+#include <cmath>
+#include <cstdint>
+#include <chrono>
+#include <random>
+
+int main() {
+    std::cout << "Base Structure" << std::endl;
+
+    return 0;
+}
